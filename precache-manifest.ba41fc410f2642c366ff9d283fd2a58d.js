fdceeaa51337a534f993db5c320c5d02680e4d6c
Derek J Leong <derekjleong@gmail.com>
2019-10-15 16:51:13 -0700

    Updates

---
+++ precache-manifest.ba41fc410f2642c366ff9d283fd2a58d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "364b7e118f10796f35fc65266795372e",
+    "url": "/2048-BTC-Edition/index.html"
+  },
+  {
+    "revision": "96b6155f6f01d9c470e1",
+    "url": "/2048-BTC-Edition/static/css/main.496ee4c5.chunk.css"
+  },
+  {
+    "revision": "b2838d3c9e4cf4694ddf",
+    "url": "/2048-BTC-Edition/static/js/2.ab533eb6.chunk.js"
+  },
+  {
+    "revision": "96b6155f6f01d9c470e1",
+    "url": "/2048-BTC-Edition/static/js/main.1f1a1245.chunk.js"
+  },
+  {
+    "revision": "858074a4900dc97ab8a0",
+    "url": "/2048-BTC-Edition/static/js/runtime~main.78dce06b.js"
+  },
+  {
+    "revision": "c6440537897819593546341d0eac5a28",
+    "url": "/2048-BTC-Edition/static/media/bitcoin.c6440537.svg"
+  }
+]);
